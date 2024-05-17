bc2884aec610ce498048b8d98ea0cb4b414a1249
wika04260 <163203050+wika04260@users.noreply.github.com>
2024-05-17 16:09:54 +0200

    Update forgevm.cxx

--- forgevm.cxx
+++ forgevm.cxx
@@ -3,5 +3,22 @@ using namespace std;
 int main()
 {
   int wpis;
-  cout << "Wybierz język/select lang";
+  string jezyk;
+  cout << "Wybierz jezyk/select lang (PL/EN)" << endl;
+  cin >> jezyk;
+  if(jezyk=="PL")
+  {
+    cout << "Witaj w ForgeVM!" << endl;
+    cout << endl;
+    cout << "Wybierz serwer" << endl;
+    cout << "Lista serwerow dostepna pod /lista" << endl;
+    cout << "Uruchomisz serwer pod /run ALIAS" << endl;
+    cout << "Wgrasz serwer pod /install ALIAS" << endl;
+    cout << "Milej zabawy z przyjaciolmi!" << endl;
+  }
+  else if(jezyk=="EN")
+  {
+    cout << "Kup se translatora!";
+    return 0;
+  }
 }
